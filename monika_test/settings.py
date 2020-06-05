54b98576274a558b5e5aaf0f8189d1394672afd7
monika-fafluch <monika-fafluch@gmail.com>
2020-06-05 23:15:19 +0200

    ticket:1 test fails

--- monika_test/settings.py
+++ monika_test/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'test42'
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,9 @@ TEMPLATES = [
         },
     },
 ]
+
+FIXTURE_DIRS = (os.path.join(BASE_DIR, 'fixtures'),)
+
 
 WSGI_APPLICATION = 'monika_test.wsgi.application'
 
