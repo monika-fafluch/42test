584e6103f1994cdf6946b3cf2244304a202dfc1f
monika-fafluch <monika-fafluch@gmail.com>
2020-06-06 19:23:02 +0200

    ticket:1 before general test

--- test42/tests.py
+++ test42/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeViewTest(TestCase):
+
+	def test_view_status(self):
+		response = self.client.get('')
+		self.assertEqual(response.status_code, 200)
